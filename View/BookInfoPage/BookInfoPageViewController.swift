8cca922e219620fd6ecee9b641c8c2c1498cbfa3
이유진 <iyujin@iyujin-ui-noteubug.local>
2024-05-03 20:56:39 +0900

    [Rename] Folder 정리

---
+++ View/BookInfoPage/BookInfoPageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BookInfoPageViewController.swift
+//  BookSearching
+//
+//  Created by 이유진 on 5/3/24.
+//
+
+import UIKit
+
+class BookInfoPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
